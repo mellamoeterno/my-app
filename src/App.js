6727cc2051f9b4c883b2623e3f65367f638a2390
Samuel Enoque <thesonlike@gmail.com>
2022-10-15 22:19:44 -0300

    appJsAndPagesHomeJsmodified

--- src/App.js
+++ src/App.js
@@ -2,13 +2,15 @@ import "./App.css";
 import { BrowserRouter as Router, Routes, Route, Link } from "react-router-dom";
 import home from "./pages/home";
 
+//to route the specific components created //
+
 function App () {
   return (
-    <Routers>
+    <Router> 
       <Routes>
-
+        <Route path="/" element={<home />}/>
       </Routes>
-    </Routers>
+    </Router>
   );
 
 }
