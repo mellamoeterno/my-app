fcfe45f1b01614b0ae3795f27457bf60e5733e98
Samuel Enoque <thesonlike@gmail.com>
2022-10-20 15:19:09 -0300

    fewChangesOnAppjsAndLoginjs

--- src/App.js
+++ src/App.js
@@ -1,14 +1,27 @@
 import "./App.css";
 import { BrowserRouter as Router, Routes, Route, Link } from "react-router-dom";
-import home from "./pages/home";
+import Home from "./pages/home";
+import CreatePost from "./pages/CreatePost";
+import Login from "./pages/Login";
+
+
+
 
 //to route the specific components created //
 
 function App () {
   return (
     <Router> 
+      <nav>
+      <Link to="/"> home </Link>
+      <Link to="/create"> create post </Link>
+      <Link to="/login"> login </Link>
+
+    </nav>
       <Routes>
-        <Route path="/" element={<home />}/>
+        <Route path="/" element={<Home />} />
+        <Route path="/CreatePost" element={<CreatePost />} />
+        <Route path="/login" element={<Login />} />
       </Routes>
     </Router>
   );
